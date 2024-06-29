dc3384c8c7e4b2aac5ca5ef019dfd442d2c06aec
anubhav100rao <anubhav100rao@gmail.com>
2024-06-29 16:57:23 +0530

    updated

--- makefile
+++ makefile
@@ -1,8 +1,10 @@
 docker-up:
-	docker-compose -f elastic-search-kibana-docker-compose.yaml up
-	docker-compose -f kafka-docker-compose.yaml up
+	docker-compose -f elastic-search-kibana-docker-compose.yaml up -d
+	docker-compose -f kafka-docker-compose.yaml up -d
+	brew services start logstash
 
 push:
 	git add .
 	git commit -m "updated"
 	git push -u origin main
+
--- producer.py
+++ producer.py
@@ -1,5 +1,7 @@
 from faker import Faker
 from kafka import KafkaProducer
+import json
+from time import sleep
 
 fake = Faker()
 
@@ -12,7 +14,19 @@ def get_registered_data():
     }
 
 
+def json_serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+
 producer = KafkaProducer(
-    bootstrap_servers='192.168.1.2:9092',
-    value_serializer=lambda v: str(v).encode('utf-8')
+    bootstrap_servers=['192.168.1.2:9092'],
+    value_serializer=json_serializer
 )
+
+
+if __name__ == '__main__':
+    while True:
+        data = get_registered_data()
+        producer.send('registered_user', value=data)
+        print(data)
+        sleep(3)
